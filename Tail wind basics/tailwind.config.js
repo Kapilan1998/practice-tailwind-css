15558d53b4418e971ac61e70a31b86096dceaa60
kapilan <sriranjankapilan@gmail.com>
2024-12-31 21:15:07 +0530

    basics of tailwind finished

--- Tail wind basics/tailwind.config.js
+++ Tail wind basics/tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '11xl': '6.956rem', // Custom font size
+      },
+      lineHeight:{
+        'kapilan-45':'2.5rem'
+      },
+      colors:{
+        'color-kapilan':"rgba(250,24,25,0.6)"
+      }
+    },
+    fontFamily: {
+      // here key is custom name and its value is existing one
+      myNewFont: ["Playwrite US Modern Guides", "serif"],
+      rubikVinyl: ["Rubik Vinyl", "serif"],
+      kapilan: ["Sixtyfour", "serif"],
+    },
   },
   plugins: [],
-}
-
+  darkMode:"selector"
+};
